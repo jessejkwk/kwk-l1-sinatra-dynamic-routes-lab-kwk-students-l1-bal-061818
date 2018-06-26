15061c4e5ffd970ebe014ef3463dfe5eb6e3e0a3
jessejkwk <janderson@nyatoday.com>
2018-06-26 17:45:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
-  # Write your code here!
-  get '/:name' do
-    @user_name = params[:name]
-    @user_name
+  class App < Sinatra::Base
+    # Write your code here!
+    get '/:name' do
+      @user_name = params[:name]
+      @user_name
+    end
   end
-end
-
-class Square < Sinatra::Base
-  get '/square/:number' do
-    @square = (@number.to_i**2).to_s
-    @square
+  
+  class Square < Sinatra::Base
+    get '/square/:number' do
+      @square = (@number.to_i**2).to_s
+      @square
+    end
   end
-end
 
 class Phrase < Sinatra::Base
   get '/say/:number/:phrase' do
